b477a70fb971f20817b0f9b7420f382f6c961c1b
jerrywzhang <jerrywzhang@tencent.com>
2020-03-01 22:29:40 +0800

    init

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
-
+import Success from '../views/Success'
+import store from '../store/index.js'
+import  Login from '../views/Login'
 Vue.use(VueRouter)
 
 const routes = [
@@ -11,12 +13,26 @@ const routes = [
     component: Home
   },
   {
+    path:'/success',
+    name:'Success',
+    meta:{
+      requireAuth: true
+    },
+    component:Success
+  },
+  {
     path: '/about',
     name: 'About',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path:'/login',
+    name:'Login',
+    component:Login
+
   }
 ]
 
@@ -24,6 +40,23 @@ const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  if (to.meta.requireAuth) {  // 判断该路由是否需要登录权限
+    if (store.state.token) {  // 通过vuex state获取当前的token是否存在
+      next();
+    }
+    else {
+      next({
+        path: '/login',
+        query: {redirect: to.fullPath}  // 将跳转的路由path作为参数，登录成功后跳转到该路由
+      })
+    }
+  }
+  else {
+    next();
+  }
 })
 
 export default router
---
+++ src/views/Success.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>Login Success ! {{userName}}</div>
+</template>
+
+<script>
+    export default {
+        name: "Success",
+
+        data () {
+            return {
+                userName:'Jerry',
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
